afb5ec80c9528a991dcabe18ae1793de6b0c5ca4
Caydn Baldwin <153124562+caydnbaldwin@users.noreply.github.com>
2024-11-12 03:46:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def game():
       # get random word and print the number of letters
       entity = u.hangman()
       print(f'Word length: {len(entity.word)} letters -> {"_" * len(entity.word)}')
+      
       # while word is not correctly guessed...
       while ''.join(entity.underscores) != entity.word:
             # guess a letter, make sure its not an already guessed letter, and increment total guesses
@@ -16,6 +17,7 @@ def game():
                         print('You play too much.')
                         exit()
             entity.total_guesses += 1
+            
             # if letter is correct, add it to correct guess set and print correct
             if entity.check_letter(letter):
                   entity.correct_guesses.append(letter)
@@ -28,8 +30,10 @@ def game():
             print(f'Total Guesses: {entity.total_guesses}')
             print(f'Correct Guesses ({len(entity.correct_guesses)}): {', '.join(entity.correct_guesses)}')
             print(f'Incorrect Guesses ({len(entity.incorrect_guesses)}): {', '.join(entity.incorrect_guesses)}\n')
+            
       # print that the user guessed the word correctly in x amount of tries
       print(f'You have correctly guessed the word, "{entity.word}", in {entity.total_guesses} tries!\n')
+      
       # ask if they want to play again, if yes, recurse, if anything else, exit
       play_again = input('Type \'y\' to play again, or \'q\' to quit: ').lower()
       if play_again == 'y':
